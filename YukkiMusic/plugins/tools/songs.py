c51ff1c4744dbd9ec90733eb288f38f49a6890a4
sonaakshi1 <97940751+sonaakshi1@users.noreply.github.com>
2022-05-20 08:15:18 +0530

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -34,20 +34,6 @@ SONG_COMMAND = get_command("SONG_COMMAND")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
-)
-@language
-async def song_commad_group(client, message: Message, _):
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["SG_B_1"],
-                    url=f"https://t.me/{app.username}?start=song",
-                ),
-            ]
-        ]
-    )
-    await message.reply_text(_["song_1"], reply_markup=upl)
 
 
 # Song Module
@@ -55,7 +41,6 @@ async def song_commad_group(client, message: Message, _):
 
 @app.on_message(
     filters.command(SONG_COMMAND)
-    & filters.private
     & ~filters.edited
     & ~BANNED_USERS
 )
